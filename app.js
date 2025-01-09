c6cfd975ae8d82c398b6e0630e5718d6d0c6ba28
Lucas Akatsuka <ljoakatsuka@hotmail.com>
2025-01-09 16:17:28 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+    effect: "cards",
+    grabCursor: true,
+    initialSlide: 2,
+    loop: true, 
+    rotate: true,
+    mousewheel: {
+      invert: false,
+    },
+  });
