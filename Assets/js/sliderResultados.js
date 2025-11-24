3a9fda8426b3f4d54af738d21b523d14ea381e56
Nicolas Henrique <nicolas.hdx8@gmail.com>
2025-11-24 01:42:19 -0300

    nova seção resultados

--- Assets/js/sliderResultados.js
+++ Assets/js/sliderResultados.js
@@ -1,25 +1,45 @@
-let nextDom = document.getElementById('next');
-let prevDom = document.getElementById('prev');
-
 let carouselDom = document.querySelector('.carousel');
 let SliderDom = carouselDom.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
 let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carousel .time');
+
+let timeRunning = 1050; 
+let timeAutoNext = 7000;
+
+let isRunning = false; 
+let runTimeOut;
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 1000;
 
-nextDom.onclick = function(){
-    showSlider('next');    
-}
+thumbnailBorderDom.addEventListener('click', (event) => {
+    const clickedItem = event.target.closest('.item');
+    if (!clickedItem || isRunning) return;
 
-prevDom.onclick = function(){
-    showSlider('prev');    
-}
-let runTimeOut;
+    const currentActiveImage = SliderDom.querySelector('.item img').src;
+    
+    const clickedImage = clickedItem.querySelector('img').src;
+    if (currentActiveImage === clickedImage) {
+        return;
+    }
+
+    const currentThumbs = document.querySelectorAll('.carousel .thumbnail .item');
+    const index = Array.from(currentThumbs).indexOf(clickedItem);
+
+    for (let i = 0; i < index; i++) {
+        let firstSlider = SliderDom.querySelector('.item');
+        let firstThumb = thumbnailBorderDom.querySelector('.item');
+        
+        SliderDom.appendChild(firstSlider);
+        thumbnailBorderDom.appendChild(firstThumb);
+    }
+    showSlider('next');
+});
+
 function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    if(isRunning === true) return; 
+    isRunning = true;
+
+    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
     
     if(type === 'next'){
@@ -31,10 +51,12 @@ function showSlider(type){
         thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
         carouselDom.classList.add('prev');
     }
+
     clearTimeout(runTimeOut);
+
     runTimeOut = setTimeout(() => {
         carouselDom.classList.remove('next');
         carouselDom.classList.remove('prev');
+        isRunning = false; 
     }, timeRunning);
-
 }
