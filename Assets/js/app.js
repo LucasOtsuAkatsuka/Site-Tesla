df718d2e7c807a141c4171ff4e6de17f2974059d
Nicolas Henrique <nicolas.hdx8@gmail.com>
2026-01-14 16:15:48 -0300

    seção competição

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper('.swiper-container', {
     effect: "cards",
     grabCursor: true,
-    initialSlide: 2,
+    initialSlide: 0,
     loop: true, 
     rotate: true,
     mousewheel: {
